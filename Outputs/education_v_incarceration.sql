9d8451aadb9b92e10d7e451e76912f5b817c1533
Katelyn Burke <katelyn.burke2@gmail.com>
2019-05-13 20:24:10 -0500

    updated sql

--- Outputs/education_v_incarceration.sql
+++ Outputs/education_v_incarceration.sql
@@ -1,13 +1,12 @@
 -- Create and use education_v_incarceration_db
+DROP DATABASE IF EXISTS ed_prison_db;
+CREATE DATABASE ed_prison_db;
 
-CREATE DATABASE education_v_incarceration_db;
-USE education_v_incarceration_db;
+USE ed_prison_db;
 
-DELETE FROM education_v_incarceration;
+DROP TABLE if exists education_v_incarceration;
 
-
-CREATE TABLE education_report (
-    id INT AUTO_INCREMENT NOT NULL,
+CREATE TABLE education_v_incarceration (
     STATE VARCHAR(20),
     YEAR INT(4),
     TOTAL_EXPENDITURE VARCHAR(20),
@@ -18,8 +17,43 @@ CREATE TABLE education_report (
     PRISONER_COUNT VARCHAR(20),
 	STATE_POPULATION VARCHAR(20),
 	VIOLENT_CRIME_TOTAL VARCHAR(20),
-    PROPERTY_CRIME_TOTAL VARCHAR(20),
-	PRIMARY KEY (id)
+    PROPERTY_CRIME_TOTAL VARCHAR(20)
 );
 
+-- Create and use education_v_incarceration_db
+Drop Database if exists  education_v_incarceration_db;
+CREATE DATABASE education_v_incarceration_db;
+USE education_v_incarceration_db;
+
+DROP TABLE if exists full_report;
+
+CREATE TABLE full_report (
+    STATE VARCHAR(20),
+    YEAR INT(4),
+    TOTAL_EXPENDITURE varchar(20),
+    INSTRUCTION_EXPENDITURE varchar(20),
+    4TH_ENROLLED varchar(20),
+    8TH_ENROLLED varchar(20),
+    TOTAL_ENROLLMENT varchar(20),
+    4_AVG_MATH_SCORE varchar(20),
+    8_AVG_MATH_SCORE varchar(20),
+    4_AVG_RDG_SCORE varchar(20),
+    8_AVG_RDG_SCORE varchar(20),
+	PRISONER_COUNT varchar(20),
+    STATE_POPULATION varchar(20),
+    VIOLENT_CRIME_TOTAL varchar(20),
+    PROPERTY_CRIME_TOTAL varchar(20),
+	TOTAL_JAIL_ADM varchar(20),
+    ASIAN_JAIL_POP  varchar(20),
+    BLACK_JAIL_POP  varchar(20),
+    LATINO_JAIL_POP  varchar(20),
+    NATIVE_JAIL_POP  varchar(20),
+    WHITE_JAIL_POP  varchar(20),
+    ASIAN_PRISON_POP  varchar(20),
+    BLACK_PRISON_POP varchar(20),
+    LATINO_PRISON_POP  varchar(20),
+    NATIVE_PRISON_POP varchar(20),
+    OTHER_PRISON_POP varchar(20),
+    WHITE_PRISON_POP varchar(20)
+);
 
